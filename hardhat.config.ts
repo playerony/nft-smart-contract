aa311c75523667db65e84cc7257bb7eb1a14d8b8
playerony <pawel.wojtasinski.1995@gmail.com>
2022-02-18 18:34:32 +0100

    chore: initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,15 @@
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
+import 'hardhat-gas-reporter';
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: '0.8.11',
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+};
